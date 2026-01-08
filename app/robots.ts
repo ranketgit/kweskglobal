a3d3e046c75b9090a131aa2ae29cbd6d2121a4ae
ranketgit <jalal@ranket.co>
2026-01-08 17:58:39 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
+      disallow: ['/api/', '/thankyou'],
     },
     sitemap: 'https://kwesk.com/sitemap.xml',
   }
